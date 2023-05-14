44d815823dafce0b5c8776b7d9b4988b5fd104e8
Marcel <bvmaru@proton.me>
2023-05-14 19:45:22 +0200

    cos tam

--- src/app/CenterPanel.java
+++ src/app/CenterPanel.java
@@ -1,2 +1,135 @@
-package app;public class CenterPanel {
+package app;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import org.freixas.jcalendar.JCalendarCombo;
+/**
+ * Program <code>MyWindow</code>
+ * Klasa <code>CenterPanel</code> definiujaca centralny panel
+ * aplikacji zawierajacy glowna funkcjonalnosc aplikacji
+ * @author
+ * @version 1.0	15/12/2010
+ */
+public class CenterPanel extends JPanel implements ActionListener {
+
+    private static final long serialVersionUID = 1L;
+    private JPanel northPanel, southPanel;
+    private JTextField paramTextField;
+    private JTextArea resultTextArea;
+    private JLabel paramLabel;
+    private JButton submitButton;
+    // deklaracja zmiennej typu JCalendarCombo o nazwie jccData
+    private JCalendarCombo jccData;
+    private TitledBorder titledBorder;
+    private Border blackLine;
+    /**
+     * Konstruktor bezparametrowy klasy <CODE>InfoBottomPanel<CODE>
+     */
+    public CenterPanel() {
+        createGUI();
+    }
+    /**
+     * Metoda tworzacaca graficzny interfejs uzyytkownika
+     */
+    public void createGUI() {
+        this.setLayout(new GridLayout(2,1,5,5));
+
+        // Utworzenie panelu z paramtrami i wynikiem
+        northPanel = createNorthPanel();
+        southPanel = createSouthPanel();
+
+        // Utworzenie obiektow TextField
+        this.add(northPanel);
+        this.add(southPanel);
+    }
+    /**
+     * Metoda tworzaca panel z parametrami
+     */
+    public JPanel createNorthPanel() {
+        JPanel jp = new JPanel();
+        blackLine = BorderFactory.createLineBorder(Color.gray);
+        titledBorder = BorderFactory.createTitledBorder(blackLine,
+                "Parmetry wejsciowe");
+        titledBorder.setTitleJustification(TitledBorder.CENTER);
+        jp.setBorder(titledBorder);
+        jp.setLayout(new FlowLayout());
+
+        paramLabel = new JLabel("Parametr 1");
+        paramTextField = new JTextField(10);
+        submitButton = new JButton("Submit");
+        submitButton.addActionListener(this);
+
+        // utworzenie instacji obiektu JCalendar
+        jccData = new JCalendarCombo(
+                Calendar.getInstance(),
+                Locale.getDefault(),
+                JCalendarCombo.DISPLAY_DATE,
+                false
+        );
+        // ustawienie formatu daty
+        jccData.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+
+        jp.add(paramLabel);
+        jp.add(paramTextField);
+        jp.add(jccData);
+        jp.add(submitButton);
+        return jp;
+    }
+    /**
+     * Metoda tworzaca panel z wynikami
+     */
+    public JPanel createSouthPanel() {
+        JPanel jp = new JPanel();
+        blackLine = BorderFactory.createLineBorder(Color.gray);
+        titledBorder = BorderFactory.createTitledBorder(blackLine,
+                "Uzyskany rezultat");
+        titledBorder.setTitleJustification(TitledBorder.CENTER);
+        jp.setBorder(titledBorder);
+        jp.setLayout(new BorderLayout());
+
+        resultTextArea = new JTextArea();
+        // zawijanie wierszy
+        resultTextArea.setLineWrap(true);
+        // edycja pola TextArea
+        // resulTextAreat.setEditable(false);
+        resultTextArea.append("Start aplikacji\n");
+        jp.add(new JScrollPane(resultTextArea),BorderLayout.CENTER);
+        return jp;
+    }
+    /**
+     * Metoda obsługujaca zdarzenie akcji
+     */
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource() == submitButton) {
+            String param = paramTextField.getText();
+            // Pobranie daty do obiektu typu String
+            // Miesiace liczone sa od 0 wiec trzeba dodac 1
+            Calendar cal = jccData.getCalendar();
+            String data = ""+cal.get(Calendar.YEAR)+"-";
+            int miesiac = cal.get(Calendar.MONTH)+1;
+            if(miesiac <= 9) data = data+"0"+String.valueOf(miesiac)+"-";
+            else data = data+String.valueOf(miesiac)+"-";
+            int dzien = cal.get(Calendar.DAY_OF_MONTH);
+            if(dzien <= 9) data = data+"0"+String.valueOf(dzien);
+            else data = data+String.valueOf(dzien);
+
+            // zapisanie danych w polu TextArea
+            resultTextArea.append("Parametr: "+param+"\n");
+            resultTextArea.append("Data: "+data+"\n");
+        }
+    }
+    /**
+     * Metoda okreslajaca wartosci odstepow od krawedzi panelu
+     * (top,left,bottom,right)
+     */
+    public Insets getInsets() {
+        return new Insets(5,10,10,10);
+    }
 }
--- src/app/MyWindow.java
+++ src/app/MyWindow.java
@@ -1,3 +1,5 @@
+package app;
+
 import java.awt.*;
 import java.awt.print.PageFormat;
 import java.awt.print.PrinterJob;
@@ -10,13 +12,13 @@ public class MyWindow extends JFrame implements ActionListener, Runnable {
     private static final int HEIGHT = 450;
     private JPanel conPane;
 
-    MyLogger logger = null;
+    //MyLogger logger = null;
     /**
      * Zmienne tworzace menu
      */
     JMenuBar menuBar;
     JMenu fileMenu, editMenu, viewMenu, calcMenu, helpMenu;
-    JMenuItem fileExitMenuItem, helpContextMenutem, helpAboutMenuItem;
+    JMenuItem fileExitMenuItem, helpContextMenuItem, helpAboutMenuItem, sumaMenuItem, sredniaMenuItem, minMenuitem, maxMenuitem;
 
     //JCheckBoxMenuItem viewStatusBarMenuItem, viewJToolBarMenuItem;
 
@@ -24,26 +26,33 @@ public class MyWindow extends JFrame implements ActionListener, Runnable {
      * Zmienne tworz�ce pasek narzedziowy
      */
     JToolBar jToolBar;
-    JButton jtbPrint, jtbExit, jtbHelp, jtbAbout;
+    JButton jtbSave, jtbSuma, jtbSrednia, jtbMin, jtbMax, jtbExit, jtbHelp, jtbAbout;
 
     /**
      * definicja etykiet tekstowych
      */
     String[] labelMenu = {"Plik", "Edycja", "Widok", "Obliczenia", "Pomoc"};
-    String[] labelFileMenuItem = {"Logowanie", "Wylogowanie", "Drukuj", "Zamknij"};
+    String[] labelFileMenuItem = {"Zamknij","Zapisz"};
+    String[] labelCalcMenuItem = {"Suma","Srednia","Min","Max"};
     String[] labelViewMenuItem = {"Ukryj pasek statusu", "Ukryj pasek narz�dziowy"};
     String[] labelHelpMenuItem = {"Kontekst pomocy", "Informacje o programie"};
 
     /**
      * definicja etykiet opisuj�cych w tzw. dymkach
      */
-    String[] tooltipFileMenu = {"Dodaj wartość do tabeli", "Wyzeruj tabelę",
+
+    String[] tooltipFileMenu ={"Zamknij","Zapisz"};
+            String[] tooltipCalcMenu = {"Dodaj wartość do tabeli", "Wyzeruj tabelę",
             "Zapisz zawartość tabeli do pliku", "Wyświetl sumę wszystkich elementów tablicy",
             "Wyświetl średnią z wszystkich elementów tablicy", "wartość min", "wartość Max"};
+
+    //String[] tooltipFileMenu = {"Dodaj wartość do tabeli", "Wyzeruj tabelę",
+      //      "Zapisz zawartość tabeli do pliku", "Wyświetl sumę wszystkich elementów tablicy",
+         //   "Wyświetl średnią z wszystkich elementów tablicy", "wartość min", "wartość Max"};
     String[] tooltipHelpMenu = {"Uruchomienie pomocy", "Informacje o programie"};
 
-    Icon iconLogin, iconLogout, iconPrint, iconExit, iconHelpContext, iconAbout;
-    Icon mIconLogin, mIconLogout, mIconPrint, mIconExit, mIconHelpContext, mIconAbout;
+    Icon iconSuma, iconSrednia, iconMin, iconMax, iconExit, iconHelpContext, iconAbout, iconSave;
+    Icon miconSuma, miconSrednia, miconMin, miconMax, mIconExit, mIconHelpContext, mIconAbout, miconSave;
 
     /**
      * Konstruktor bezparametrowy klasy <code>MyWindow</code>
@@ -53,7 +62,7 @@ public class MyWindow extends JFrame implements ActionListener, Runnable {
         // definicja zdarzenia zamkniecia okna
         this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                MyLogger.writeLog("INFO", "Zamkniecie aplikacji");
+                //MyLogger.writeLog("INFO", "Zamkniecie aplikacji");
                 dispose();
                 System.exit(0);
             }
@@ -71,7 +80,7 @@ public class MyWindow extends JFrame implements ActionListener, Runnable {
         conPane = (JPanel) this.getContentPane();
         conPane.setLayout(new BorderLayout());
         // Utworzenie dziennika zdarzen
-        logger = new MyLogger();
+        //logger = new MyLogger();
 
         // utworzenie GUI w watku zdarzeniowym
         try {
@@ -83,7 +92,7 @@ public class MyWindow extends JFrame implements ActionListener, Runnable {
                 }
             });
         } catch (Exception e) {
-            MyLogger.writeLog("ERROR", "Blad podczas tworzenia GUI aplikacji");
+            //MyLogger.writeLog("ERROR", "Blad podczas tworzenia GUI aplikacji");
             System.out.println("ERROR - Blad podczas tworzenia GUI aplikacji");
         }
         // utworzenie watku uruchamiajacgo okno logowania
@@ -91,33 +100,178 @@ public class MyWindow extends JFrame implements ActionListener, Runnable {
         thread.start();
     }
 
+    public void run() {
+        try {
+            Thread.sleep(1000); // opoznienie 1 sekunda
+        }
+        catch(InterruptedException e) {}
+        // uruchomienie okna logowania
+    }
+
     private void createIcons() {
         try {
             // Utworzenie ikon 24 * 24 px dla paska toolbar
-            iconLogin = createMyIcon("login.jpg");
-            iconLogout = createMyIcon("logout.jpg");
-            iconPrint = createMyIcon("print.jpg");
+            iconSuma = createMyIcon("suma.jpg");
+            iconSrednia = createMyIcon("srednia.jpg");
+            iconMin = createMyIcon("min.jpg");
+            iconMax = createMyIcon("max.jpg");
             iconExit = createMyIcon("close.jpg");
             iconHelpContext = createMyIcon("help_context.jpg");
             iconAbout = createMyIcon("about.jpg");
+            iconSave = createMyIcon("save.jpg");
 
             // Utworzenie ikon 16 * 16 px dla MenuItem
-            mIconLogin = createMyIcon("min_login.jpg");
-            mIconLogout = createMyIcon("min_logout.jpg");
-            mIconPrint = createMyIcon("min_print.jpg");
+            miconSuma = createMyIcon("min_suma.jpg");
+            miconSrednia = createMyIcon("min_srednia.jpg");
+            miconMin = createMyIcon("min_min.jpg");
+            miconMax = createMyIcon("min_max.jpg");
             mIconExit = createMyIcon("min_close.jpg");
             mIconHelpContext = createMyIcon("min_help_context.jpg");
             mIconAbout = createMyIcon("min_about.jpg");
+            miconSave = createMyIcon("min_save.jpg");
         }
         catch(Exception e) {
-            MyLogger.writeLog("ERROR","Blad tworzenia ikon");
+            //MyLogger.writeLog("ERROR","Blad tworzenia ikon");
             System.out.println("ERROR - Blad tworzenia ikon");
         }
+    }
+
+    private void createMenu() {
+        // Utworzenie paska menu
+        menuBar = new JMenuBar();
+        // Utworzenie pol menu glownego
+        fileMenu = createJMenu(labelMenu[0], KeyEvent.VK_P, true);
+        editMenu = createJMenu(labelMenu[1], KeyEvent.VK_E, false);
+        viewMenu = createJMenu(labelMenu[2], KeyEvent.VK_W, false);
+        calcMenu = createJMenu(labelMenu[3], KeyEvent.VK_W, true);
+        helpMenu = createJMenu(labelMenu[4], KeyEvent.VK_O, true);
+
+        // Utworzenie pol podmenu
+
+        fileExitMenuItem = createJMenuItem(labelFileMenuItem[0],mIconExit,
+                KeyStroke.getKeyStroke(KeyEvent.VK_X,ActionEvent.ALT_MASK),true);
+
+        // utworzenie MenuItem dla calc
+        sumaMenuItem = createJMenuItem(labelCalcMenuItem[0],miconSuma,
+                KeyStroke.getKeyStroke(KeyEvent.VK_L,ActionEvent.ALT_MASK),true);
+        sredniaMenuItem = createJMenuItem(labelCalcMenuItem[1],miconSrednia,
+                KeyStroke.getKeyStroke(KeyEvent.VK_W,ActionEvent.ALT_MASK),false);
+        minMenuitem = createJMenuItem(labelCalcMenuItem[2],miconMin,
+                KeyStroke.getKeyStroke(KeyEvent.VK_D,ActionEvent.ALT_MASK),false);
+        maxMenuitem = createJMenuItem(labelCalcMenuItem[3],miconMax,
+                KeyStroke.getKeyStroke(KeyEvent.VK_F,ActionEvent.ALT_MASK),false);
+
+        // utworzenie obiektow MenuItem dla helpMenu
+        helpContextMenuItem = createJMenuItem(labelHelpMenuItem[0],
+                mIconHelpContext,KeyStroke.getKeyStroke(KeyEvent.VK_H,
+                        ActionEvent.ALT_MASK),true);
+        helpAboutMenuItem = createJMenuItem(labelHelpMenuItem[1],mIconAbout,
+                KeyStroke.getKeyStroke(KeyEvent.VK_I,ActionEvent.ALT_MASK),true);
+
+        // dodanie utworzonych elementow menu dopaska menu JMenuBar
+        fileMenu.add(fileExitMenuItem);
+        calcMenu.add(sumaMenuItem);
+        calcMenu.add(sredniaMenuItem);
+        calcMenu.add(minMenuitem);
+        calcMenu.add(maxMenuitem);
+        helpMenu.add(helpContextMenuItem);
+        helpMenu.add(helpAboutMenuItem);
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(viewMenu);
+        menuBar.add(calcMenu);
+        menuBar.add(helpMenu);
+        this.setJMenuBar(menuBar);
+    }
+
+    private void createGUI() {
+
+        // Utworzenie paska narzedziowego
+        jToolBar = new JToolBar();
+        createJToolBar(jToolBar);
+        // Utworzenie panelu informacyjnego umieszczonego na dole okna
+        InfoBottomPanel infoPanel = new InfoBottomPanel();
+        // Utworzenie panelu centralnego
+        CenterPanel centerPanel = new CenterPanel();
+        centerPanel.setVisible(true);
+
+        conPane.add(jToolBar, BorderLayout.NORTH);
+        conPane.add(infoPanel, BorderLayout.SOUTH);
+        conPane.add(centerPanel, BorderLayout.CENTER);
+
+    }
+
+    public void createJToolBar(JToolBar cjtb) {
+        cjtb.setFloatable(false);
+        cjtb.add(Box.createHorizontalStrut(5));
+
+        // Utworzenie przycisk�w paska narz�dziowego
+        jtbExit = createJButtonToolBar(tooltipFileMenu[0],iconExit,true);
+        jtbSave = createJButtonToolBar(tooltipFileMenu[1],iconSave,true);
+
+        jtbSuma = createJButtonToolBar(tooltipCalcMenu[0],iconSuma,true);
+        jtbSrednia = createJButtonToolBar(tooltipCalcMenu[1],iconSrednia,true);
+        jtbMin = createJButtonToolBar(tooltipCalcMenu[2],iconMin,true);
+        jtbMax = createJButtonToolBar(tooltipCalcMenu[3],iconMax,true);
+
+        jtbHelp = createJButtonToolBar(tooltipHelpMenu[0],iconHelpContext,true);
+        jtbAbout = createJButtonToolBar(tooltipHelpMenu[1],iconAbout,true);
+
+        // dodanie przycisk�w do paska narz�dziowego
+        cjtb.add(jtbExit);
+        cjtb.add(jtbSave);
+        cjtb.addSeparator();
+        cjtb.add(jtbSuma);
+        cjtb.add(jtbSrednia);
+        cjtb.add(jtbMin);
+        cjtb.add(jtbMax);
+        cjtb.addSeparator();
+        cjtb.add(jtbHelp);
+        cjtb.add(jtbAbout);
     }
 
     public Icon createMyIcon(String file) {
         String name = "/grafika/"+file;
         Icon icon = new ImageIcon(getClass().getResource(name));
         return icon;
+    }
+
+    public JMenu createJMenu(String name, int keyEvent, boolean enable) {
+        JMenu jMenu = new JMenu(name);
+        jMenu.setMnemonic(keyEvent);
+        jMenu.setEnabled(enable);
+        return jMenu;
+    }
+
+    public JMenuItem createJMenuItem(String name, Icon icon, KeyStroke key,
+                                     boolean enable) {
+        JMenuItem jMI;
+        if(icon != null)
+            jMI = new JMenuItem(name,icon);
+        else jMI = new JMenuItem(name);
+        jMI.setAccelerator(key);
+        jMI.addActionListener(this);
+        jMI.setEnabled(enable);
+        return jMI;
+    }
+
+    public JButton createJButtonToolBar(String tooltip,Icon icon,boolean enable) {
+        JButton jb = new JButton("",icon);
+        jb.setToolTipText(tooltip);
+        jb.addActionListener(this);
+        jb.setEnabled(enable);
+        return jb;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+    }
+
+    public static void main(String args[])
+    {
+        //MyLogger.writeLog("INFO","Start Aplikacji");
+        System.out.println("Start Aplikacji");
+        MyWindow f = new MyWindow();
+        f.setVisible(true);
     }
 }
